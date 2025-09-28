f1a028a353d38d2fa7aae6f3af8a4efdfd2546b2
vinay techdev <vinayvinuu629@gmail.com>
2025-09-28 15:39:12 +0530

    chore: initial commit

---
+++ javScript.js
@@ -0,0 +1,29 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents"); 
+
+function opentab(tabname){
+    for (tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+
+    for (tabcontent of tabcontents){
+       tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+var sideMenu=document.getElementById("side-menu");
+
+ function openmenu(){
+    sideMenu.style.right="0";
+ }
+
+ function closemenu(){
+    sideMenu.style.right="-200px";
+ }
+
+
+ 
